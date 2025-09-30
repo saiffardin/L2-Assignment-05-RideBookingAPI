fc899113dbdc79906995995719d0d31e857b6737
Saif Chowdhury <saiffardin@gmail.com>
2025-09-30 13:38:52 +0600

    Remove dist files, added to git ignore

--- dist/app/constants/enum.roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role["DRIVER"] = "DRIVER";
-    Role["RIDER"] = "RIDER";
-})(Role || (exports.Role = Role = {}));
--- dist/app/constants/enum.vehicle-type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VehicleType = void 0;
-var VehicleType;
-(function (VehicleType) {
-    VehicleType["CAR"] = "CAR";
-    VehicleType["BIKE"] = "BIKE";
-})(VehicleType || (exports.VehicleType = VehicleType = {}));
